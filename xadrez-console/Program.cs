e65aff6e7f64bf0c91ffe15de4d030804f18a76a
Nicolas Capelari <nicolasaugusto777@hotmail.com>
2022-02-17 20:26:26 -0300

    Jogo Completo

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,44 @@
+﻿using tabuleiro;
+using xadrez_console;
+using xadrez;
+
+try
+{
+    PartidaDeXadrez partida = new PartidaDeXadrez();
+
+    while (!partida.terminada)
+    {
+        try { 
+        Console.Clear();
+        Tela.imprimirPartida(partida);
+
+        Console.Write("Origem: ");
+        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+        partida.validarPosicaoDeOrigem(origem);
+
+        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+        Console.Write("Destino: ");
+        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+            partida.validarPosicaoDeDestino(origem, destino);
+
+        partida.realizaJogada(origem, destino);
+        }
+        catch (TabuleiroException ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.ReadKey();
+        }
+    }
+    Console.Clear() ;
+    Tela.imprimirPartida(partida);
+}
+catch (TabuleiroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadKey();
